d92ca62a8ee3875abbc6d882440c62391ffbc2c2
Bhanu Pratap Singh <33947900+Bhanugkp@users.noreply.github.com>
2024-05-30 12:42:36 +0530

    Update tree.h

--- tree/tree.h
+++ tree/tree.h
@@ -53,61 +53,6 @@ void setRight(nodeptr p, char x) {
   p->right = makeTree(x);
   p->right->father = p; // Set the father pointer
 }
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
-  char info;
-  struct Node *left;
-  struct Node *right;
-  struct Node *father;
-};
-
-typedef struct Node *nodeptr;
-
-nodeptr getNode() {
-  nodeptr q = (nodeptr)malloc(sizeof(struct Node));
-  if (q == NULL) {
-    printf("Memory allocation failed\n");
-    exit(1);
-  }
-  return q;
-}
-
-nodeptr makeTree(char x) {
-  nodeptr q = getNode();
-  q->info = x;
-  q->right = q->left = NULL;
-  q->father = NULL; // Initialize father as NULL
-  return q;
-}
-
-void setLeft(nodeptr p, char x) {
-  if (p == NULL) {
-    printf("Cannot insert into null node\n");
-    return;
-  }
-    
-  if (p->left != NULL) {
-    printf("Left child already exists\n");
-    return; 
-  }
-  p->left = makeTree(x);
-  p->left->father = p; // Set the father pointer
-}
-
-void setRight(nodeptr p, char x) {
-  if (p == NULL) {
-    printf("Cannot insert into null node\n");
-    return; 
-  }
-  if (p->right != NULL) {
-    printf("Right child already exists\n");
-    return; 
-  }
-  p->right = makeTree(x);
-  p->right->father = p; // Set the father pointer
-}
 
 nodeptr left(nodeptr p) {
         return p->left;
